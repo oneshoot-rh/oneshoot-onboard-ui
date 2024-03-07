2f9511e082451bd49db7c97cb5dadb3fad468b68
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-07 21:57:38 +0100

    update: new components as subroutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,24 @@ import { EmployeeOnboardingComponent } from './components/employee-onboarding/em
 import { AutomationComponent } from './components/automation/automation.component';
 import { GenaiComponent } from './components/genai/genai.component';
 import { EContractsComponent } from './components/e-contracts/e-contracts.component';
+import { EmployeesComponent } from './components/employee-management/employees/employees.component';
+import { JobsComponent } from './components/employee-management/jobs/jobs.component';
+import { DepartmentsComponent } from './components/employee-management/departments/departments.component';
+import { BenchmarksComponent } from './components/employee-management/benchmarks/benchmarks.component';
+import { InternshipsComponent } from './components/employee-management/internships/internships.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'employee-management', component: EmployeeManagementComponent },
+    { path: 'employee-management', component: EmployeeManagementComponent,
+        children:[
+            { path: '', redirectTo: 'employees', pathMatch: 'full' },
+            { path: 'employees', component: EmployeesComponent},
+            { path: 'departments', component: DepartmentsComponent},
+            { path: 'jobs', component: JobsComponent},
+            { path: 'benchmarks', component: BenchmarksComponent},
+            { path: 'internships', component: InternshipsComponent}
+        ]
+    },
     { path: 'employee-onboarding', component: EmployeeOnboardingComponent },
     { path: 'automation', component: AutomationComponent },
     { path: 'gen-ai', component: GenaiComponent },
