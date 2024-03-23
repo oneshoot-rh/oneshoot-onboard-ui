904141cc5f986b94a80cefad54b1e5deca2bce25
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-23 23:15:31 +0000

    update ui

--- src/app/components/charts/employee-stats/employee-stats.component.ts
+++ src/app/components/charts/employee-stats/employee-stats.component.ts
@@ -74,27 +74,6 @@ export class EmployeeStatsComponent implements OnInit{
       }
       
     });
-
-    this.chart_3 = new Chart("chart_3", {
-      type: 'pie', //this denotes tha type of chart
-
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-                 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-         datasets: [
-          {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-                 '574', '573', '576'],
-            backgroundColor: ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown']
-          } 
-        ]
-      },
-      options: {
-        aspectRatio:2.5
-      }
-      
-    });
   }
 
 }
