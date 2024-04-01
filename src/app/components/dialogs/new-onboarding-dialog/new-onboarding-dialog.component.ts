d47663192418a26d2e3323d58c443ae3881d4277
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-04-01 15:33:12 +0000

    consumed some rest apis to construct onboardingPlan request and submit it to the backend

--- src/app/components/dialogs/new-onboarding-dialog/new-onboarding-dialog.component.ts
+++ src/app/components/dialogs/new-onboarding-dialog/new-onboarding-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatPseudoCheckbox } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,6 +12,11 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import { JobOfferService } from '../../../services/employees/job-offer.service';
+import { MailServiceService } from '../../../services/mail/mail-service.service';
+import { CandidateService } from '../../../services/candidates/candidate.service';
+import { OnboardingService } from '../../../services/candidates/onboarding.service';
+import { onboardingPlanRequest } from '../../../types/types';
 
 
 @Component({
@@ -33,59 +38,85 @@ import {MatButtonModule} from '@angular/material/button';
   templateUrl: './new-onboarding-dialog.component.html',
   styleUrl: './new-onboarding-dialog.component.css'
 })
-export class NewOnboardingDialogComponent {
+export class NewOnboardingDialogComponent implements OnInit {
   public searchQuery: string = '';
   public onboardingSubject: string = '';
-  public onboardingDateTime: string = '';
+  public onboardingDate: Date = new Date(); 
   public notifyCandidates: boolean = false;
-  public selectedTemplate: string = '';
-  public emailSubject: string = '';
-  public selectedJobOffer: string = '';
-  public jobOffers: any = [
-    {
-      title: 'Job Offer 1',
-    },
-    {
-      title: 'Job Offer 2',
-    },
-    {
-      title: 'Job Offer 3',
-    }
-  ];
-  public emailTemplates: any = [
-    {
-      name: 'Template 1',
-    },
-    {
-      name: 'Template 2',
-    },
-    {
-      name: 'Template 3',
-    }
-  ];
-  public data: any = [
-    {
-      name: 'John Doe',
-      position: 'Software Engineer',
-    },
-    {
-      name: 'mounika',
-      position: 'Software Engineer',
-    }
-  ];
+  public selectedTemplate: number = 0; 
+  public emailSubjectName: string = '';  
+  public selectedJobOffer: number = 0; 
+  public jobOffers: any = [];
+  public emailTemplates: any = [];
+  public data: any = [];
   public filteredData: any = [];
   selectedCandidates: any[] = [];
 
 
+  constructor(
+    private _jobOfferService : JobOfferService,
+    private _mailService: MailServiceService,
+    private candidateService: CandidateService,
+    private _onboardingService: OnboardingService
+  ) { }
 
   public addOnboarding() {
-    console.log('New Onboarding Added');
+    // date should be in this format yyyy-MM-dd-HH-mm-ss
+    const onDate = this.onboardingDate.toISOString().split('.')[0];
+    const onboardingPlanRequest : onboardingPlanRequest= {
+      onboardingSubject: this.onboardingSubject,
+      onboardingDate: onDate,
+      notifyCandidates: this.notifyCandidates,
+      emailTemplateId: this.selectedTemplate,
+      emailSubjectName: this.emailSubjectName,
+      jobOfferId: this.selectedJobOffer,
+      candidatesIds: this.selectedCandidates.map((candidate: any) => candidate.id)
+    };
+    console.log('request',onboardingPlanRequest);
+    this._onboardingService.addOnboardingPlan(onboardingPlanRequest).subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+
   }
 
   public filterData() {
     this.filteredData = this.data.filter((item: any) => {
-      return item.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+      const fullName = item.firstName + ' ' + item.lastName;
+      return fullName.toLowerCase().includes(this.searchQuery.toLowerCase());
     });
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('onboardingPlanDraft')) {
+      const draft = JSON.parse(localStorage.getItem('onboardingPlanDraft') || '{}');
+      this.onboardingSubject = draft.onboardingSubject;
+      this.onboardingDate = draft.onboardingDate;
+      this.notifyCandidates = draft.notifyCandidates;
+      this.selectedTemplate = draft.selectedTemplate;
+      this.emailSubjectName = draft.emailSubjectName;
+      this.selectedJobOffer = draft.selectedJobOffer;
+      this.selectedCandidates = draft.selectedCandidates;
+    }
+    this._jobOfferService.getJobOffers().subscribe(
+      data => {
+        console.log(data);
+        this.jobOffers = data;
+      }
+    );
+    this._mailService.getEmailTemplates().subscribe(
+      data => {
+        console.log(data);
+        this.emailTemplates = data;
+      }
+    );
+    this.candidateService.getCandidates().subscribe(
+      data => {
+        console.log(data.content);
+        this.data = data.content;
+      }
+    );
   }
 
   handleSelectionChange(selectionList: MatSelectionList) {
@@ -103,7 +134,17 @@ export class NewOnboardingDialogComponent {
   }
 
   public saveAsDraft() {
-    console.log('New Onboarding Saved as Draft');
+    // save in local storage
+    const draft = {
+      onboardingSubject: this.onboardingSubject,
+      onboardingDate: this.onboardingDate,
+      notifyCandidates: this.notifyCandidates,
+      selectedTemplate: this.selectedTemplate,
+      emailSubjectName: this.emailSubjectName,
+      selectedJobOffer: this.selectedJobOffer,
+      selectedCandidates: this.selectedCandidates
+    };
+    localStorage.setItem('onboardingPlanDraft', JSON.stringify(draft));
   }
 
 
